121178170f1788672176bd15b3f0ca54393a3a1e
Shubham Srineth <61264644+shubham-singh-21@users.noreply.github.com>
2020-07-01 17:25:00 +0530

    Add files via upload

---
+++ merging.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import xarray as xr
+
+merged_variable = []
+file = open('filename.txt')#read the filename and filename must be in correctorde
+for i in file:
+  d1 = i.strip('\n')
+  d = xr.open_dataset(d1)
+  variables = d['var130'][:,35,:,:]
+  merged_variable.append(variables)
+
+data1 = xr.concat(g_p,dim='time')
+data1.to_netcdf('newfile.nc',format='NETCDF4')
