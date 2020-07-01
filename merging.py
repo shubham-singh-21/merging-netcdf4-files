0ec39351a7bd0984eb9509e5cd8eb9ae89b5b3fb
Shubham Srineth <61264644+shubham-singh-21@users.noreply.github.com>
2020-07-01 17:27:00 +0530

    Update merging.py

--- merging.py
+++ merging.py
@@ -2,12 +2,12 @@ import pandas as pd
 import xarray as xr
 
 merged_variable = []
-file = open('filename.txt')#read the filename and filename must be in correctorder
+file = open('filename.txt')#read the filename and filename must be in correct order
 for i in file:
   d1 = i.strip('\n')
   d = xr.open_dataset(d1)
   variables = d['var130'][:,35,:,:]
   merged_variable.append(variables)
 
-data1 = xr.concat(g_p,dim='time')
+data1 = xr.concat(merged_variable,dim='time')
 data1.to_netcdf('newfile.nc',format='NETCDF4')
